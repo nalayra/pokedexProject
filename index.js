b65f1f13b927e7af4d57205dc98424d65fb8dd3a
sofversoza <92960864+sofversoza@users.noreply.github.com>
2021-12-10 09:55:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,112 @@
-//API URL for list of all pokemon
-const URL = `https://pokeapi.co/api/v2/pokemon/`;
-
-//constants identifyers
-const mainForm = document.querySelector("#main-form")
-let mainImage = mainForm.querySelector("img")
-const pokemonName = mainForm.querySelector("h4")
-
-//constants references
-const pokedex = document.getElementById("pokedex");
-const promises = [];  //this creates 'promises' array
-  
-//big boy fetch
-const fetchPokemon = () => {
-
-//gets URL for each pokemon and puts it in 'promises' array
-for (let i = 1; i < 9; i++){
-    promises.push(fetch(`${URL}${i}`)
-    .then(res => res.json()))
-}
-
-//adds in all of the info for each pokemon into 'promises' array
-Promise.all(promises).then(data => {
-
-    const pokemon = data.map( (result, index) => ({
-        name: result.name,
-        id: index + 1,
-        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`,
-        apiURL: result.forms[0].url,
-        type: result.types.map(type => type.type.name).join(', ')
-    }))
-
-    console.log(data)
-    console.log(pokemon)
-    displayPokemon(pokemon);
-});
+const poke_container = document.querySelector('#poke_container')
+const pokemons_number = 150;
+const typeColors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5',
 };
 
-fetchPokemon();
-//! The li was referenced in the HTML file tag as a <ul>, so I made updates so I could style via CSS
-const displayPokemon = pokemon => {
-    const pokemonHTML = pokemon.map ( poke => `
-    <li class="card" onclick="selectPokemon(${poke.id})">
-        <img class="card-image" src="${poke.image}"/>
-        <h2 class="card-title" >${poke.id}. ${poke.name}</h2>
-        <p class="card-subtitle">Type: ${poke.type}</p>
-    </li>
-    `)
-    .join('');
-    pokedex.innerHTML = pokemonHTML;
+const mainTypes = Object.keys(typeColors);
+
+const fetchPokemons = async () => {
+    for (let i = 1; i <= pokemons_number; i++) {
+        await fetchAPokemon(i);
+    }
 }
 
-const selectPokemon = async (id) => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-    const res = await fetch (url);
-    const poke = await res.json();
-    updatePicture(poke);
+const fetchAPokemon = async id => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url);
+    const pokemon = await res.json();
+    createPokeCard(pokemon);
+}
+fetchPokemons();
+
+function createPokeCard(pokemon) {
+    const pokemonElement = document.createElement('div');
+    pokemonElement.classList.add('pokemon');
+    
+    const poke_types = pokemon.types.map(data => data.type.name)
+    const type = mainTypes.find(type => poke_types.indexOf(type) > -1);
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+    const color = typeColors[type];
+    pokemonElement.style.backgroundColor = color;
+
+    const pokeInnerHTML = `
+        <div class='img-container'>
+            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokemon.id}.png'
+        </div>
+        <div class='info'>
+            <h3 class='name'>${name}</h3>
+            <small class='type'>Type: <span>${type}</span></small>
+        </div>
+        <span class='number'>#${pokemon.id.toString().padStart(3, '0')}</span>
+    `;
+    pokemonElement.innerHTML = pokeInnerHTML;
+    poke_container.appendChild(pokemonElement);
 }
 
-//updates the main picture to the one you click on
-const updatePicture = (poke) => {
-    mainImage.src = poke.sprites['front_default']   
+
+//----------------------SCROLL TO TOP BUTTON--------------//
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+
+//-------------ADDED FROM ORIGINAL PROJECT---------------//
+
+const li = document.createElement("favorites-list")
+//^ <div> #id variables from HTML
+const randomPoke = document.getElementById("pokemon-randomize");
+const randomBox = document.getElementById("randomize-box");
+
+const favoritePoke = document.getElementById("pokemon-favorites");
+const favoriteBox = document.getElementById("favorites-box");
+    favoritesBox.append(li)
+
+const searchPoke = document.getElementById("pokemon-search")
+
+//^ eventListener for randomPoke
+        //^ CREATE ONE SUBMIT EVENT LISTENER FOR BOTH BUTTONS?
+randomPoke.addEventListener('submit', (event) => {
+     event.preventDefault();
+})
+
+//^ eventListener for favoritePoke
+favoritePoke.addEventListener('submit', (event) => {
+     event.preventDefault();
+})
+//^ eventListener for searchPoke
+searchPoke.addEventListener('click', () => {
+
+})
